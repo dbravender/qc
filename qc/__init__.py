34614b539c4d4d64cb6cb5b27bea61f99585d6ad
Dan Bravender <dan.bravender@gmail.com>
2009-07-31 14:51:26 +0900

    Removed redundant tests

--- qc/__init__.py
+++ qc/__init__.py
@@ -3,8 +3,6 @@ import types
 import os
 from functools import partial
 
-__all__ = ['integers', 'lists', 'unicodes', 'characters', 'forall']
-
 def evaluate(lazy_value):
     while hasattr(lazy_value, '__call__'):
         lazy_value = lazy_value()
@@ -49,62 +47,4 @@ def forall(tries=100, **kwargs):
     return wrap
 forall.verbose = False # if enabled will print out the random test cases
 
-@forall(tries=10, i=integers)
-def test_integers(i):
-    assert type(i) == int
-    assert i >= 0 and i <= 100
-
-@forall(tries=10, l=lists(items=integers))
-def test_a_int_list(l):
-    assert type(l) == list
-
-@forall(tries=10, ul=lists(items=unicodes))
-def test_unicodes_list(ul):
-    assert type(ul) == list
-    if len(ul):
-        assert type(ul[0]) == unicode
-
-@forall(tries=10, l=lists(items=integers, size=(10, 50)))
-def test_lists_size(l):
-    assert len(l) <= 50 and len(l) >= 10
-
-@forall(tries=10, u=unicodes)
-def test_unicodes(u):
-    assert type(u) == unicode
-
-@forall(tries=10, u=unicodes(size=(1,1)))
-def test_unicodes_size(u):
-    assert len(u) == 1
-
-def random_int_unicode_tuple():
-    return lambda: (evaluate(integers), evaluate(unicodes))
-
-@forall(tries=10, l=lists(items=random_int_unicode_tuple))
-def test_a_tupled_list(l):
-    for x in l:
-        assert type(x[0]) == int and type(x[1]) == unicode
-
-@forall(tries=10, x=integers, y=integers)
-def test_addition_associative(x, y):
-    assert x + y == y + x
-
-@forall(tries=10, l=lists)
-def test_reverse_reverse(l):
-    assert list(reversed(list(reversed(l)))) == l
-
-@forall(tries=10, c=characters)
-def test_characters(c):
-    assert len(c) == 1
-
-@forall(tries=10, d=dicts(items=unicodes, values=integers))
-def test_dicts(d):
-    for x, y in d.iteritems():
-        assert type(x) == unicode
-        assert type(y) == int
-
-@forall(tries=10, d=dicts(items=unicodes, values=lists, size=(2, 2)))
-def test_dicts_size(d):
-    assert len(d) == 2
-    for x, y in d.iteritems():
-        assert type(x) == unicode
-        assert type(y) == list
+__all__ = ['integers', 'lists', 'unicodes', 'characters', 'forall']
