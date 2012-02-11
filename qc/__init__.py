c06c19d161c76a7dd18b2aadc6d95e31f5091ec0
Dan Bravender <dan.bravender@gmail.com>
2012-02-12 03:51:22 +0900

    PEP-8 cleanup

--- qc/__init__.py
+++ qc/__init__.py
@@ -4,6 +4,7 @@ import random
 import os
 import functools
 
+
 def integers(low=0, high=100):
     '''Endlessly yields random integers between (inclusively) low and high.
        Yields low then high first to test boundary conditions.
@@ -13,6 +14,7 @@ def integers(low=0, high=100):
     while True:
         yield random.randint(low, high)
 
+
 def floats(low=0.0, high=100.0):
     '''Endlessly yields random floats between (inclusively) low and high.
        Yields low then high first to test boundary conditions.
@@ -21,6 +23,7 @@ def floats(low=0.0, high=100.0):
     yield high
     while True:
         yield random.uniform(low, high)
+
 
 def lists(items=integers(), size=(0, 100)):
     '''Endlessly yields random lists varying in size between size[0]
@@ -32,6 +35,7 @@ def lists(items=integers(), size=(0, 100)):
     while True:
         yield [next(items) for _ in range(random.randint(size[0], size[1]))]
 
+
 def tuples(items=integers(), size=(0, 100)):
     '''Endlessly yields random tuples varying in size between size[0]
        and size[1]. Yields a tuple of the low size and the high size
@@ -40,11 +44,14 @@ def tuples(items=integers(), size=(0, 100)):
     yield tuple([next(items) for _ in range(size[0])])
     yield tuple([next(items) for _ in range(size[1])])
     while True:
-        yield tuple([next(items) for _ in range(random.randint(size[0], size[1]))])
+        yield tuple([next(items)
+            for _ in range(random.randint(size[0], size[1]))])
+
 
 def key_value_generator(keys=integers(), values=integers()):
     while True:
         yield [next(keys), next(values)]
+
 
 def dicts(key_values=key_value_generator(), size=(0, 100)):
     while True:
@@ -56,6 +63,7 @@ def dicts(key_values=key_value_generator(), size=(0, 100)):
             x.update({item: value})
         yield x
 
+
 def unicodes(size=(0, 100), minunicode=0, maxunicode=255):
     for r in (size[0], size[1]):
         yield ''.join(chr(random.randint(minunicode, maxunicode)) \
@@ -65,6 +73,7 @@ def unicodes(size=(0, 100), minunicode=0, maxunicode=255):
                 for _ in range(random.randint(size[0], size[1])))
 
 characters = functools.partial(unicodes, size=(1, 1))
+
 
 def objects(_object_class, _fields={}, *init_args, **init_kwargs):
     ''' Endlessly yields objects of given class, with fields specified
@@ -78,6 +87,7 @@ def objects(_object_class, _fields={}, *init_args, **init_kwargs):
         for k, v in _fields.items():
             setattr(obj, k, next(v))
         yield obj
+
 
 def forall(tries=100, **kwargs):
     def wrap(f):
@@ -93,6 +103,9 @@ def forall(tries=100, **kwargs):
                 f(*inargs, **random_kwargs)
         return wrapped
     return wrap
-forall.verbose = False # if enabled will print out the random test cases
+forall.verbose = False  # if enabled will print out the random test cases
 
-__all__ = ['integers', 'floats', 'lists', 'tuples', 'unicodes', 'characters', 'objects', 'forall']
+
+__all__ = [
+    'integers', 'floats', 'lists', 'tuples', 'unicodes', 'characters',
+    'objects', 'forall']
--- tests/test_qc.py
+++ tests/test_qc.py
@@ -1,22 +1,29 @@
-from qc import integers, floats, unicodes, characters, lists, tuples, dicts, objects, forall
+from qc import (
+    integers, floats, unicodes, characters, lists, tuples, dicts, objects,
+    forall)
+
 
 @forall(tries=10, i=integers())
 def test_integers(i):
     assert type(i) == int
     assert i >= 0 and i <= 100
 
+
 @forall(tries=10, l=lists(items=integers()))
 def test_a_int_list(l):
     assert type(l) == list
+
 
 @forall(tries=10, l=tuples(items=integers()))
 def test_a_int_tuple(l):
     assert type(l) == tuple
 
+
 @forall(tries=10, i=floats())
 def test_floats(i):
     assert type(i) == float
     assert i >= 0.0 and i <= 100.0
+
 
 @forall(tries=10, l=lists(items=floats()))
 def test_a_float_list(l):
@@ -24,27 +31,33 @@ def test_a_float_list(l):
     for x in l:
         assert type(x) == float
 
+
 @forall(tries=10, ul=lists(items=unicodes()))
 def test_unicodes_list(ul):
     assert type(ul) == list
     if len(ul):
         assert type(ul[0]) == str
 
+
 @forall(tries=10, l=lists(items=integers(), size=(10, 50)))
 def test_lists_size(l):
     assert len(l) <= 50 and len(l) >= 10
+
 
 @forall(tries=10, l=tuples(items=integers(), size=(10, 50)))
 def test_tuples_size(l):
     assert len(l) <= 50 and len(l) >= 10
 
+
 @forall(tries=10, u=unicodes())
 def test_unicodes(u):
     assert type(u) == str
 
-@forall(tries=10, u=unicodes(size=(1,1)))
+
+@forall(tries=10, u=unicodes(size=(1, 1)))
 def test_unicodes_size(u):
     assert len(u) == 1
+
 
 def random_int_str_tuple():
     i = integers()
@@ -52,22 +65,27 @@ def random_int_str_tuple():
     while True:
         yield (next(i), next(u))
 
+
 @forall(tries=10, l=lists(items=random_int_str_tuple()))
 def test_a_tupled_list(l):
     for x in l:
         assert type(x[0]) == int and type(x[1]) == str
 
+
 @forall(tries=10, x=integers(), y=integers())
 def test_addition_commutative(x, y):
     assert x + y == y + x
+
 
 @forall(tries=10, l=lists())
 def test_reverse_reverse(l):
     assert list(reversed(list(reversed(l)))) == l
 
+
 @forall(tries=10, c=characters())
 def test_characters(c):
     assert len(c) == 1
+
 
 def kv_str_integers():
     u = unicodes()
@@ -75,17 +93,20 @@ def kv_str_integers():
     while True:
         yield (next(u), next(i))
 
+
 @forall(tries=10, d=dicts(key_values=kv_str_integers()))
 def test_dicts(d):
     for x, y in d.items():
         assert type(x) == str
         assert type(y) == int
 
+
 def kv_unicodes_lists():
     u = unicodes()
     l = lists()
     while True:
         yield (next(u), next(l))
+
 
 @forall(tries=10, d=dicts(key_values=kv_unicodes_lists(), size=(2, 2)))
 def test_dicts_size(d):
@@ -94,9 +115,11 @@ def test_dicts_size(d):
         assert type(x) == str
         assert type(y) == list
 
+
 @forall(tries=10, i=integers(low=0, high=10))
 def each_integer_from_0_to_10(i, target_low, target_high):
-    assert i >= target_low and i<= target_high
+    assert i >= target_low and i <= target_high
+
 
 def test_qc_partials():
     each_integer_from_0_to_10(target_low=0, target_high=10)
@@ -106,10 +129,11 @@ class TestClass(object):
     def __init__(self, arg):
         self.arg_from_init = arg
 
-@forall(tries=10, obj=objects(TestClass, {'an_int': integers(), 'a_float': floats()}, unicodes()))
+
+@forall(tries=10, obj=objects(TestClass, {
+    'an_int': integers(), 'a_float': floats()}, unicodes()))
 def test_objects(obj):
     assert type(obj) == TestClass
     assert type(obj.an_int) == int
     assert type(obj.a_float) == float
     assert type(obj.arg_from_init) == str
-
